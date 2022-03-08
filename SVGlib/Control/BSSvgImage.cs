8e7b669ef47916dd0bc4eba3507c297c9b90a185
UnderKo <hhit934@gmail.com>
2022-03-08 21:10:29 +0300

    A

---
+++ SVGlib/Control/BSSvgImage.cs
@@ -0,0 +1,83 @@
+﻿using SGVlib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SVGlib.Control
+{
+    public class BSSvgImage : Border
+    {
+        Grid grid = new Grid();
+        SGVparser parser = new SGVparser();
+        public BSSvgImage()
+        {
+            this.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            this.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;        
+            this.Child = grid;
+            #region event s
+
+            this.SizeChanged += BSSvgImage_SizeChanged;
+            #endregion
+        }
+
+        private void BSSvgImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeSvg();
+        }
+
+        public string Source
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+                SetSvg(value);
+                
+            }
+        }
+        
+        public string Content
+        {
+            get; set;
+        }
+        private void SetSvg(string path)
+        {
+            if (!File.Exists(path))
+                return;
+
+            Content = File.ReadAllText(path);
+
+            parser.OpenSVGString(Content);
+            parser.Parse();
+            grid.Children.Clear();
+
+            foreach (SVG_PATH svg in parser.BASESVG.svgpaths)
+            {
+                System.Windows.Shapes.Path path_svg = new System.Windows.Shapes.Path();
+
+                path_svg.Data = Geometry.Parse(svg.d);
+                path_svg.Fill = Brushes.White;
+                path_svg.PathResize(this.Width, this.Height);
+                 
+                grid.Children.Add(path_svg);
+            }
+        }
+        public void ResizeSvg()
+        {
+            foreach (System.Windows.Shapes.Path path_svg in grid.Children)
+            {
+                double w_ = parser.BASESVG.width.To();
+                double h_ = parser.BASESVG.height.To();
+                path_svg.PathResize(this.Width / w_, this.Height / h_);
+            }
+        }
+    }
+}
---
+++ SVGlib/Control/ControlStaticLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SVGlib.Control
+{
+    public static class ControlStaticLib
+    {
+        public static void PathResize(this System.Windows.Shapes.Path tr, double w, double h)
+        {
+            ScaleTransform myScaleTransform = new ScaleTransform();
+            
+            myScaleTransform.ScaleX = w;
+            myScaleTransform.ScaleY = h;
+            TransformGroup myTransformGroup = new TransformGroup();
+            myTransformGroup.Children.Add(myScaleTransform);
+            tr.RenderTransform = myTransformGroup;
+        }
+    }
+}
